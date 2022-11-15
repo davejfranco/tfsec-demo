a0c3a129f90a49f50a421e39e1d5c67b8e4d1776
Dave <davefranco1987@gmail.com>
2022-11-15 23:05:07 +0100

    tfsec demo

--- main.tf
+++ main.tf
@@ -1,18 +1,25 @@
 data "aws_region" "current" {}
 
+#tfsec:ignore:aws-ec2-enforce-http-token-imds
 resource "aws_instance" "web" {
   ami           = "ami-005e54dee72cc1d00"
   instance_type = "t3.micro"
 
+
+  root_block_device {
+    encrypted = true
+  }
   tags = {
     Name        = "webserver"
     Environment = "local"
   }
 }
 
+#tfsec:ignore:aws-ec2-enforce-http-token-imds
 resource "aws_instance" "db" {
   ami           = "ami-005e54dee72cc1d00"
   instance_type = "t3.micro"
+
 
   tags = {
     Name        = "MysqlDB"
